25b8a68f8e1a979de92fc312058387f931b149f1
EasonMa1135 <yitong.ma1135@gmail.com>
2025-09-24 22:22:37 -0700

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,20 @@
+import json
+from datetime import datetime
+from apps import create_app
+from apps.orm import *
+from gevent import pywsgi
+import logging
 
+app = create_app()
+
+@app.route('/')
+def home():
+    return 'Hello, Flask is running!'
+
+from apps import db
+
+topic = 'testapp'
+
+if __name__ == '__main__':
+    server = pywsgi.WSGIServer(('0.0.0.0', 8000), app)
+    server.serve_forever()
